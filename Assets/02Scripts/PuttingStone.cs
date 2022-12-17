ccab23544a612fda29edf6c2582829efa92a09f1
Lee An <leean200600@gmail.com>
2022-12-17 19:36:59 +0900

    ff

--- Assets/02Scripts/PuttingStone.cs
+++ Assets/02Scripts/PuttingStone.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class PuttingStone : MonoBehaviour
 {
     Vector3 mousePos;
@@ -13,28 +13,42 @@ public class PuttingStone : MonoBehaviour
     int layerMask;
     int BlackOrWhite;
     public RuleCheck rule;
+    GameObject stone;
+    Vector3 buildPos;
+    public Text trunOrder;
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
+    {
+        beforeConfirmed();
+    }
+    void beforeConfirmed()
     {
         layerMask = 1 << LayerMask.NameToLayer("Stone");
         hit = Physics2D.Raycast(new Vector3(mousePos.x, mousePos.y), Vector3.forward, 100, layerMask);
         mousePos = Input.mousePosition;
         mousePos = cam.ScreenToWorldPoint(mousePos);
 
-        if (Input.GetKey(KeyCode.Mouse0) && !hit)
+        if (Input.GetKey(KeyCode.Mouse0) && !hit && !(mousePos.x > 9.5f || mousePos.x < -8.5 || mousePos.y > 9.5 || mousePos.y < -8.5))
         {
+            GameObject.Destroy(stone);
             offsetX = mousePos.x >= 0 ? 0.5f : -0.5f;
             offsetY = mousePos.y >= 0 ? 0.5f : -0.5f;
-            Vector3 bulidPos = new Vector3((int)mousePos.x + offsetX, (int)mousePos.y + offsetY);
-            //건물 건설
-            GameObject structure = Instantiate(BlackOrWhite == 0? stone_B : stone_W, bulidPos, stone_B.transform.rotation);
-            rule.checkerboard[(int)((int)mousePos.y + offsetY + 8.5f),(int)((int)mousePos.x + offsetX + 8.5f)] = BlackOrWhite;
-            BlackOrWhite = BlackOrWhite == 0 ? 1 : 0;
+            buildPos = new Vector3((int)mousePos.x + offsetX, (int)mousePos.y + offsetY);
+            stone = Instantiate(BlackOrWhite == 0 ? stone_B : stone_W, buildPos, stone_B.transform.rotation);
+
+            stone.GetComponent<SpriteRenderer>().color = new Color(stone.GetComponent<SpriteRenderer>().color.r, stone.GetComponent<SpriteRenderer>().color.g, stone.GetComponent<SpriteRenderer>().color.b, 0.2f);
+            //BlackOrWhite = BlackOrWhite == 0 ? 1 : 0;
         }
+    }
+    public void posConfirmed()
+    {
+        GameObject.Destroy(stone);
+        Instantiate(BlackOrWhite == 0 ? stone_B : stone_W, buildPos, stone_B.transform.rotation);
+        rule.checkerboard[(int)(buildPos.y + 8.5f), (int)(buildPos.x + 8.5f)] = BlackOrWhite + 1;
+        trunOrder.text = trunOrder.text == "Black" ? "White" : "Black";
+        BlackOrWhite = BlackOrWhite == 0 ? 1 : 0;
     }
 }
